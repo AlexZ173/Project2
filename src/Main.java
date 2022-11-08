fe3889ed7742d83db5f6a750e8af1491fc49701f
Alexz <alexander.zakharov73@yandex.ru>
2022-11-08 15:32:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+// Задание 1
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int milePrice = 20;
+        int miles = ticketPrice / milePrice;
+        System.out.println(" количество миль : " + miles);
+
+   // Задание 2
+        int amount = 1100;
+        int present = 100;
+        int bonus = amount / present;
+        int balance = 350;
+                if (amount >= 1000) {
+                    int summa = amount + bonus + balance;
+                    System.out.println("остаток на счету : " + summa);
+
+                }
+                else {
+                    System.out.println("остаток на счету: "+ balance);
+
+                }
+        System.out.println();
+
+    }
+
+
+
+
+
+
+
+    }
+
